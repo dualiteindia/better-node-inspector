24cef7dd29d333a6253479862662c1d57042f0c2
vaibhavityagi <vaibhatyagi5@gmail.com>
2024-10-11 20:58:00 +0530

    sending selection id to frontend

--- plugin-src/code.ts
+++ plugin-src/code.ts
@@ -1,20 +1,20 @@
 figma.showUI(__html__, { themeColors: true, height: 300 });
 
-figma.ui.onmessage = (msg) => {
-  if (msg.type === "create-rectangles") {
-    const nodes = [];
+figma.on("selectionchange", () => {
+  console.clear();
+  console.log("Figma Plugin is running");
 
-    for (let i = 0; i < msg.count; i++) {
-      const rect = figma.createRectangle();
-      rect.x = i * 150;
-      rect.fills = [{ type: "SOLID", color: { r: 1, g: 0.5, b: 0 } }];
-      figma.currentPage.appendChild(rect);
-      nodes.push(rect);
-    }
+  const selectedNodes = figma.currentPage.selection;
+  console.log(selectedNodes[0]);
+  figma.ui.postMessage({
+    type: "selection-changed",
+    data: selectedNodes[0],
+  });
+});
 
-    figma.currentPage.selection = nodes;
-    figma.viewport.scrollAndZoomIntoView(nodes);
-  }
+// figma.ui.onmessage = (msg) => {
+//   if (msg.type === "generate-tree") {
+//   }
 
-  figma.closePlugin();
-};
+//   figma.closePlugin();
+// };
--- ui-src/App.tsx
+++ ui-src/App.tsx
@@ -1,35 +1,62 @@
 import "./App.css";
-import { RouterProvider, createMemoryRouter } from "react-router-dom";
-import Rectangle from "./screen/Rectangle";
-import Home from "./screen/Home";
-import Splash from "./screen/Splash";
-import StoreProvider from "./StoreProvider";
+// import { RouterProvider, createMemoryRouter } from "react-router-dom";
+// import Rectangle from "./screen/Rectangle";
+// import Home from "./screen/Home";
+// import Splash from "./screen/Splash";
+// import StoreProvider from "./StoreProvider";
 
-const routes = [
-  {
-    path: "/",
-    element: <Splash />,
-  },
-  {
-    path: "/home",
-    element: <Home />,
-  },
-  {
-    path: "/rectangle",
-    element: <Rectangle />,
-  },
-];
+// const routes = [
+//   {
+//     path: "/",
+//     element: <Splash />,
+//   },
+//   {
+//     path: "/home",
+//     element: <Home />,
+//   },
+//   {
+//     path: "/rectangle",
+//     element: <Rectangle />,
+//   },
+// ];
 
-const router = createMemoryRouter(routes, {
-  initialEntries: ["/"],
-  initialIndex: 0,
-});
+// const router = createMemoryRouter(routes, {
+//   initialEntries: ["/"],
+//   initialIndex: 0,
+// });
+
+// function App() {
+//   return (
+//     <StoreProvider>
+//       <RouterProvider router={router} />
+//     </StoreProvider>
+//   );
+// }
+
+// export default App;
+
+// my code
+
+import { useEffect, useState } from "react";
 
 function App() {
+  const [data, setData] = useState({});
+  useEffect(() => {
+    window.onmessage = (event) => {
+      const { type, data } = event.data.pluginMessage;
+
+      console.log(data);
+
+      if (type === "selection-changed") {
+        setData(data);
+      }
+    };
+  }, []);
   return (
-    <StoreProvider>
-      <RouterProvider router={router} />
-    </StoreProvider>
+    <div>
+      <h1>Hello world</h1>
+      <p>{JSON.stringify(data)}</p>
+    </div>
   );
 }
 
